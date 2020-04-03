6578590cbc161a62280563214768149bf6eee0b3
Ivan Mushketyk <ivan.mushketyk@consensys.net>
2020-04-03 12:22:30 +0100

    Fixes tests

--- packages/server/test/app.e2e-spec.ts
+++ packages/server/test/app.e2e-spec.ts
@@ -19,6 +19,8 @@ describe('AppController (e2e)', () => {
     return request(app.getHttpServer())
       .get('/')
       .expect(200)
-      .expect('Hello World!');
+      .expect({
+        msg: 'Hello World!'
+      });
   });
 });
