94b24b2387814b6c8075ac29fa79a767abbb4908
Ivan Mushketyk <ivan.mushketyk@consensys.net>
2020-04-03 11:55:15 +0100

    Adds Swagger support

--- packages/server/src/app.controller.ts
+++ packages/server/src/app.controller.ts
@@ -1,12 +1,21 @@
 import { Controller, Get } from '@nestjs/common';
+import {ApiProperty, ApiResponse} from '@nestjs/swagger';
 import { AppService } from './app.service';
+
+class HelloWorldResponse {
+  @ApiProperty()
+  msg: string
+}
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @ApiResponse({ status: 200, type: HelloWorldResponse })
+  getHello(): HelloWorldResponse {
+    return {
+      msg: this.appService.getHello()
+    };
   }
 }
