f442115d8eb120d6423812c97f6a8833411c0d0b
Ivan Mushketyk <ivan.mushketyk@consensys.net>
2020-04-04 12:17:10 +0100

    Configures pino logging
    Additionally:
    * Resolves conflict between @types/mocha and @types/jest in tsconfig.json.
    * Enables pretty printed logs for backend in dev mode

--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import createLogger from "./observability/logger";
+
+const logger = createLogger('nestApp')
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
 
   const options = new DocumentBuilder()
       .setTitle('PandAid')
@@ -15,6 +17,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('swagger', app, document);
 
+  logger.info('Listening...')
   await app.listen(3000);
 }
 bootstrap();
---
+++ packages/server/src/observability/logger.ts
@@ -0,0 +1,13 @@
+import * as pino from 'pino';
+
+const baseLogger = pino({
+    name: 'pandaid',
+    level: 'info',
+    prettyPrint: process.env.ENVIRONMENT === 'dev',
+    // Generate ISO timestamp
+    timestamp: pino.stdTimeFunctions.isoTime
+});
+
+export default function createLogger(loggerName: string): pino.Logger {
+    return baseLogger.child({ module: loggerName });
+}
