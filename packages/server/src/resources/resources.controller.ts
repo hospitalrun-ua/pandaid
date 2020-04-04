7b498d26977b610cb6cc66bb3feb3dc09f97385a
Denys Vark <vark.den@gmail.com>
2020-04-04 09:23:04 +0300

    add api prefix

--- packages/server/src/resources/resources.controller.ts
+++ packages/server/src/resources/resources.controller.ts
@@ -1,7 +1,27 @@
-import {Controller, HttpCode, Post} from '@nestjs/common';
+import {Controller, Get, HttpCode, Post} from '@nestjs/common';
+import {ApiProperty, ApiResponse} from "@nestjs/swagger";
 
-@Controller('resources')
+class ResourcesResponse {
+    @ApiProperty()
+    list: Resource[]
+}
+
+interface Resource {
+    name: string
+}
+
+@Controller('api/resources')
 export class ResourcesController {
+    @Get()
+    @ApiResponse({ status: 200, type: ResourcesResponse })
+    public getHello(): ResourcesResponse {
+        return {
+            list: [{
+                name: "маски"
+            }]
+        };
+    }
+
     @Post()
     @HttpCode(204)
     public create() {
