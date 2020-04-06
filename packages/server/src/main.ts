e2e993ff4093a34b3da2d8c4f906235bc5fe9e1a
Ivan Mushketyk <ivan.mushketyk@consensys.net>
2020-04-06 10:42:13 +0100

    Uses pino logger to log Nest.js log messages

--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -3,11 +3,14 @@ import { ValidationPipe } from '@nestjs/common';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import createLogger from './observability/logger';
+import {PinoLoggerService} from "./observability/LoggerService";
 
 const logger = createLogger('nestApp');
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule);
+    const app = await NestFactory.create(AppModule, {
+        logger: new PinoLoggerService(logger),
+    });
     app.setGlobalPrefix('api');
     app.useGlobalPipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }));
 
---
+++ packages/server/src/observability/LoggerService.ts
@@ -0,0 +1,32 @@
+import { LoggerService } from '@nestjs/common';
+import * as pino from 'pino';
+
+export class PinoLoggerService implements LoggerService {
+    private logger: pino.Logger;
+
+    constructor(logger: pino.Logger) {
+        this.logger = logger;
+    }
+
+    log(message: string) {
+        this.logger.info(message);
+    }
+
+    error(message: string, trace: string) {
+        this.logger.error(message, {
+            trace
+        });
+    }
+
+    warn(message: string) {
+        this.logger.warn(message);
+    }
+
+    debug(message: string) {
+        this.logger.debug(message);
+    }
+
+    verbose(message: string) {
+        this.logger.debug(message);
+    }
+}
