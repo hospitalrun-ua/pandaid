c232f8334a919ee385500f8c633034c8f9b8e52e
Yevhen Badorov <badjorv@gmail.com>
2020-04-07 19:35:48 +0300

    update test for ResourcesController

--- packages/server/test/resources.e2e-spec.ts
+++ packages/server/test/resources.e2e-spec.ts
@@ -1,3 +1,30 @@
-describe('ResourcesController (e2e)', () => {
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import * as request from 'supertest';
+import { AppModule } from './../src/app.module';
+import { JwtAuthGuard } from './../src/auth/jwt-auth.guard';
+import { RolesGuard } from './../src/auth/roles.guard';
 
+describe('ResourcesController (e2e)', () => {
+    let app: INestApplication;
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    })
+    .overrideGuard(JwtAuthGuard)
+    .useValue({ canActivate: () => true })
+    .overrideGuard(RolesGuard)
+    .useValue({ canActivate: () => true })
+    .compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+
+  it('/resources (GET)', () => {
+    return request(app.getHttpServer())
+      .get('/resources')
+      .expect(200);
+  });
 });
