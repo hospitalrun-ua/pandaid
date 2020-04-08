4dd5057492f4ab173e8597fa9eb0e2fd6880c85a
Yevhen Badorov <badjorv@gmail.com>
2020-04-08 22:18:52 +0300

    add response payload check to resources test

--- packages/server/test/resources.e2e-spec.ts
+++ packages/server/test/resources.e2e-spec.ts
@@ -4,6 +4,8 @@ import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
 import { JwtAuthGuard } from './../src/auth/jwt-auth.guard';
 import { RolesGuard } from './../src/auth/roles.guard';
+import * as mockData from "../src/resources/resources.mock.json";
+import { ResourcesResponse } from '../src/resources/resources.dto';
 
 describe('ResourcesController (e2e)', () => {
     let app: INestApplication;
@@ -25,6 +27,11 @@ describe('ResourcesController (e2e)', () => {
   it('/resources (GET)', () => {
     return request(app.getHttpServer())
       .get('/resources')
-      .expect(200);
+      .expect(200)
+      .expect(() => {
+        const responseMock = new ResourcesResponse();
+        responseMock.list = mockData;
+        return responseMock;
+      });
   });
 });
